df9d11cba792fd9ab6e92d4161926454c64e6e58
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-03-12 19:22:01 +0100

    feeds, fix #1

---
+++ dmdb/feeds.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from django.contrib.syndication.views import Feed
+from django.core.urlresolvers import reverse_lazy
+
+from .models import BlogEntry
+
+
+class BlogEntriesFeed(Feed):
+    title = 'News'
+    link = reverse_lazy('dmdb:feed')
+    description = 'Latest blog entries'
+
+    def items(self):
+        return BlogEntry.objects.all()[:10]
+
+    def item_description(self, item):
+        return item.content
+
+    def item_author_name(self, item):
+        return item.author
+
+    def item_pubdate(self, item):
+        return datetime(*item.date.timetuple()[:3])
--- dmdb/urls.py
+++ dmdb/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
+from .feeds import BlogEntriesFeed
 from .views import BlogEntryDetailView, BlogEntryListView, BlogEntryLongURLRedirectView, BlogEntryShortURLRedirectView
 
 app_name = 'dmdb'
 urlpatterns = [
+    url(r'^feed$', BlogEntriesFeed(), name='feed'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[^/]+)$', BlogEntryLongURLRedirectView.as_view()),
     url(r'^(?P<pk>[0-9A-F]+)$', BlogEntryShortURLRedirectView.as_view(), name='short'),
     url(r'^(?P<slug>[^/]+)$', BlogEntryDetailView.as_view(), name='entry'),
